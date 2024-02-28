5271a9374fe46d3bef4432c2921dc20241a1c2f2
dheerajup98 <dheerajup98@gmail.com>
2024-02-28 12:03:27 +0530

    type conversion

---
+++ type_conversion.js
@@ -0,0 +1,19 @@
+let myName = "225";
+
+let myNameNumber = Number(myName);
+
+
+// "330" => true "222avbc" => Nan
+
+let myNo = 556;
+let myNoString =  String(myNo);
+
+
+let supposeBool = "dccjc";
+
+let changeBool = Boolean(supposeBool)
+
+
+// "" => false "abc" => true true => 1
+/// false => 0
+console.log(changeBool)
