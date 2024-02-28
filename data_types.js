9a47268c8b801674ebf4ff5d8285d9533ad04127
dheerajup98 <dheerajup98@gmail.com>
2024-02-28 11:29:53 +0530

    data types complete

---
+++ data_types.js
@@ -0,0 +1,35 @@
+"use strict"; // treats we are using newer version of javascript
+
+
+// Data types 
+
+// primitive data types
+
+// string
+// undefined
+// null
+// number
+// big int
+// symbol
+// boolean
+
+// Refernce data types
+
+// object
+// array
+// Date
+// Function
+
+
+const name = "dheeraj";
+let myCash;
+const value = null;
+const no = 22565;
+const isLogged = true;
+
+
+console.log(typeof name);
+console.log(typeof value);
+console.log(typeof no);
+console.log(typeof isLogged);
+console.log(typeof myCash);
