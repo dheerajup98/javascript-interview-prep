b4d9ebc2a818ab45779aa046d97ea9b71ff9aa37
dheerajup98 <dheerajup98@gmail.com>
2024-02-29 09:19:44 +0530

    string

---
+++ string.js
@@ -0,0 +1,23 @@
+let myScore = 122;
+
+let myName = "dheeraj";
+
+console.log(`my name is ${myName} and my score is an ${myScore}`);
+
+// first way to declare string 
+
+const dku = "Dheeraj kumar upadhyay";
+
+// 2nd way 
+
+let hw = new String("Hello world");
+
+console.log(hw.toUpperCase())
+
+const newStr = hw.split(' ');
+console.log(newStr);
+
+
+const n = hw.replace('world','duniya');
+console.log(n);
+
