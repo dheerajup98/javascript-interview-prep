83861eb663d5e1edce910f23fa46cb136dbfa0f9
dheerajup98 <dheerajup98@gmail.com>
2024-02-29 09:43:26 +0530

    math-number

---
+++ math_number.js
@@ -0,0 +1,33 @@
+// map
+
+// console.log(Math.abs(-45));
+
+// console.log(Math.round(65.5));
+// console.log(Math.max(5,8,6,77));
+
+// console.log(Math.min(4,8,10,3,1));
+// console.log(Math.floor(22.6));
+// console.log(Math.ceil(25.1));
+
+// console.log(Math.random());
+
+let max = 50;
+let min = 20;
+
+// console.log(Math.floor(Math.random(max-min))+min)
+
+
+// Number
+
+let myNum = new Number(556.6);
+
+//console.log(myNum.toString());
+console.log(myNum.toFixed(2));
+// 5566.00
+
+console.log(myNum.toPrecision(3));
+// 557
+
+console.log(myNum.toLocaleString(","));
+
+
