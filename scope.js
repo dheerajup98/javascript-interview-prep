5245167f83001b8e2293df387ed8bbf67c7da34e
dheerajup98 <dheerajup98@gmail.com>
2024-03-06 18:05:20 +0530

    scope

---
+++ scope.js
@@ -0,0 +1,19 @@
+// const a = 10;
+// let b = 20;
+// var c = 30;
+
+// console.table([a,b,c]);
+
+{
+    const a = 10;
+    let b = 20;
+    var c = 30;
+}
+
+// a and b is not defined give error becuase let and const is an block scope they can't access outside the block.
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+// But c is an access outside the block scope because var is an global scope.
