7ab1cdfe8603d776a640c37fbcd804b46970ee48
dheerajup98 <dheerajup98@gmail.com>
2024-03-07 10:53:16 +0530

    iife

---
+++ iife.js
@@ -0,0 +1,23 @@
+// console.log("immediately invoked function expression");
+
+
+// IIFE means to create a private scope for variables, preventing them from polluting the global scope.
+
+
+(function iife(){
+    console.log("function called immediately");
+}) ();
+
+// named iife
+
+
+(()=>{
+   console.log("normal iife");
+})();
+
+// normal iife
+
+
+((name)=>{
+  console.log(`my name is ${name}`);
+})("dheeraj");
