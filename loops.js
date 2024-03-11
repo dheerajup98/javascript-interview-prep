90ca3878a38ff4716bccbc708cad74b050078ba7
dheerajup98 <dheerajup98@gmail.com>
2024-03-11 10:30:42 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,46 @@
+for (let i=1;i<=20;i++)
+{
+    // console.log(`the value of i is ${i}`);
+}
+
+// break
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(`the value  of i is ${i}`);
+//     if(i===5)
+//     break;
+// }
+
+// for(let i=1;i<=10;i++)
+// {
+//     if(i===5){
+//     console.log("detected 5");
+//     continue;
+//     }
+// console.log(`the value of i is ${i}`);
+
+// }
+
+
+// while loop
+
+
+// let score =  1;
+
+// while(score<=10)
+// {
+//     console.log(score);
+//     score++;
+// }
+
+// do-while
+
+let i =1;
+
+do{
+    console.log("dheeraj");
+    i++;
+}while(i<5);
+
+
