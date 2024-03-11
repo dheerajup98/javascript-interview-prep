99f88f68c60b595bf48ba7125e257b008a69011e
dheerajup98 <dheerajup98@gmail.com>
2024-03-11 09:33:10 +0530

    truthy-falsy

---
+++ truthy_false.js
@@ -0,0 +1,28 @@
+// falsy values
+
+// false, "", 0 , -0, undefined, null, 0n;
+
+// truthy values
+
+// " ", '0' , 'false', [], {}, function(){};
+
+
+const myNum = [];
+
+if(myNum.length === 0)
+{
+    console.log("array is empty");
+}
+
+
+// obejct is empty or not
+
+const myObj = {}
+
+if(Object.keys(myObj).length === 0){
+    console.log("object is empty");
+}
+
+// object.key() return array.
+
+
