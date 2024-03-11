0c6ebf82e6482e871ee089aef71f748c009cc8de
dheerajup98 <dheerajup98@gmail.com>
2024-03-11 09:40:03 +0530

    nullish-colleacion

---
+++ ternary.js
@@ -0,0 +1,17 @@
+const icecream = 200;
+
+icecream >= 300 ? console.log("more tha 300" ) : console.log("less than 200");
+
+
+// nullish coleascing operator used for undefined, null;
+
+
+let val1;
+
+val1 = 10 ?? 20;
+
+val1 = undefined ?? 50;
+
+val1 = null ?? 89;
+
+console.log(val1)
